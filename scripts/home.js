9ed7ef05f2443380197e7723eb6979077d3906c5
rajkumarbaba12062000 <rajkumarbaba12062000@gmail.com>
2021-04-16 21:02:24 +0530

    v2.0

--- scripts/home.js
+++ scripts/home.js
@@ -14,4 +14,30 @@ document.body.addEventListener('click', (e) => {
             document.querySelector('.sidebarnav').classList.remove('sidebarnavactive');
         }
     }
-})
+});
+
+let k=0;
+const rollText = document.querySelectorAll('.second-text h2');
+console.log(rollText[0].style)
+setInterval(() => {
+    if(k==0) {
+        rollText.forEach(r => {
+            r.style.transform = "translateY(-100%)";
+        });
+        
+    } else if (k==1) {
+        rollText.forEach(r => {
+            r.style.transform = "translateY(-200%)";
+        });
+        
+    } else if (k==2) {
+        rollText.forEach(r => {
+            r.style.transform = "translateY(-300%)";
+        });
+    } else {
+        rollText.forEach(r => {
+            r.style.transform = "translateY(-0%)";
+        });
+    }
+    k=(k+1)%5
+}, 3000);
